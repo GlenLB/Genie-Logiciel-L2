583211bf72f8377904608ea2509a7ee0d49eb994
Glen Le Baill <lebaill.glen@gmail.com>
2019-01-31 20:14:23 +0100

    Ajout des commentaires de la méthode AnalysePage2

--- src/library/AnalysePage2.scala
+++ src/library/AnalysePage2.scala
@@ -1,6 +1,18 @@
 package library
 
+/**
+ *  Implémente l'interface library.AnalysePage /
+ */
 class AnalysePage2 extends library.AnalysePage {
+    /**
+     * A partir d'une URL de requête sur le site de référence et d'une expression exp,
+     * retourne des pages issues de la requête et satisfaisant l'expression.
+     *
+     * @param url l'URL de la requête sur le site de référence
+     * @param exp l'expression à vérifier sur les pages trouvées
+     * @return la liste des couples (titre,ref) où ref est l'URL d'une page
+     * satisfaisant l'expression et titre est son titre.
+     */
     def resultats(url: String, exp: Expression): List[(String, String)] = {
         val s: Html = UrlProcessor.fetch(url)
         var filtrageUrl = new FiltrageURL()
@@ -27,7 +39,9 @@ class AnalysePage2 extends library.AnalysePage {
         cople
     }
 
-    //rend titre d un html
+    /**
+     * @return le titre d'un document HTML
+     */
     private def titreHtml(h: Html): String = {
         var res: String = ""
         h match {
@@ -40,7 +54,10 @@ class AnalysePage2 extends library.AnalysePage {
         res
     }
 
-    //il faut qu'un texte ds la balise Title
+    /**
+     * Méthode utile à la méthode titreHtml()
+     * @return le premier texte rencontré dans une liste de HTML
+     */
     private def titreRec(n: List[Html]): String = {
         var res: String = ""
         for (f <- n) {
@@ -51,4 +68,5 @@ class AnalysePage2 extends library.AnalysePage {
         }
         res
     }
+
 }
